c0ec67734bc515c2e9bb639052025baaa185fb11
Jonathan Heaney <jonathan.heaney@gmail.com>
2023-03-02 19:14:38 -0600

    Finish studio

--- script.js
+++ script.js
@@ -19,6 +19,30 @@ window.addEventListener('load', function () {
         const nameHeader = document.createElement('h3');
         nameHeader.textContent = `${astronaut.firstName} ${astronaut.lastName}`;
         bioDiv.appendChild(nameHeader);
+        const unorderedList = document.createElement('ul');
+        bioDiv.appendChild(unorderedList);
+        const hoursInSpace = document.createElement('li');
+        hoursInSpace.textContent = `Hours in space: ${astronaut.hoursInSpace}`;
+        unorderedList.appendChild(hoursInSpace);
+        const active = document.createElement('li');
+        active.textContent = `Active: ${astronaut.active}`;
+        unorderedList.appendChild(active);
+        const skills = document.createElement('li');
+        let skillsContent = '';
+        const skillsArray = astronaut.skills;
+        for (let i = 0; i < skillsArray.length; i++) {
+          if (i === skillsArray.length - 1) {
+            skillsContent += skillsArray[i];
+          } else {
+            skillsContent += `${skillsArray[i]}, `;
+          }
+        }
+        skills.textContent = `Skills: ${skillsContent}`;
+        unorderedList.appendChild(skills);
+        const image = document.createElement('img');
+        image.classList.add('avatar');
+        image.setAttribute('src', astronaut.picture);
+        astronautDiv.appendChild(image);
       }
     });
   });
